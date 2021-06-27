2711762e5b84b342024f35104f70ecc9645921d4
Saurabh Bharti <saurabhbharti9@gmail.com>
2021-06-27 18:01:07 +0530

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer class="footer mt-auto py-3 bg-dark">
-      <div class="container">
-        <span class="text-muted">@Copyright 2021</span>
+      <div class="container footer_text">
+        <p class="text-muted">@Copyright 2021. All rights reserved</p>
       </div>
     </footer>
   );
